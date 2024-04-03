d3b14d3f188c8fc3485c2d311ba83a847e4024c8
Anuraag Reddy <anuraagreddy74@outlook.com>
2024-04-03 16:41:39 -0400

    Basic config

---
+++ lua/keymaps.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " "
+
+local map = vim.keymap.set
+
+--- Map netrw to leader pv
+map('n', '<leader>pv', vim.cmd.Ex)
+
+-- Ctrl + l to remove highlgihts and redraw screen
+map('n', '<C-l>', ':nohlsearch<cr>', defaults)
---
+++ lua/settings.lua
@@ -0,0 +1,7 @@
+local opt = vim.opt
+
+-- Basic setup for lines
+opt.cursorline = true
+opt.number = true
+opt.relativenumber = true
+opt.termguicolors = true
