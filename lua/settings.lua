460c60cb0ec2b4efdfedb7b243120307f12688b7
Anuraag Reddy <anuraagreddy74@outlook.com>
2024-04-06 00:35:41 -0400

    Added fold support

--- lua/settings.lua
+++ lua/settings.lua
@@ -13,3 +13,15 @@ opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
+opt.smarttab = true
+
+-- Indents
+opt.smartindent = true
+
+-- Linewrap
+opt.wrap = false
+
+-- Folding
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldenable = false
