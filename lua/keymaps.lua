1e1ac627ad149ca5b12b4073b24f1bb002147b86
Anuraag Reddy <anuraagreddy74@outlook.com>
2024-04-05 22:04:30 -0400

    Made some changes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 local map = vim.keymap.set
 
---- Map netrw to leader pv
+-- Map netrw to leader pv
 map('n', '<leader>pv', vim.cmd.Ex)
 
 -- Ctrl + l to remove highlgihts and redraw screen
--- lua/settings.lua
+++ lua/settings.lua
@@ -5,3 +5,11 @@ opt.cursorline = true
 opt.number = true
 opt.relativenumber = true
 opt.termguicolors = true
+
+-- Tabs
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+vim.api.nvim_set_hl(0, 'CursorLine', { underline = true })
